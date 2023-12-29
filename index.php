fb332a2d997e732c8f2130cd0007aa4818e09f99
WAUTHION Eric <eric.wauthion@etnic.be>
2023-12-29 21:13:20 +0100

    tester la connexion

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 require_once('../../config.php');
-
 $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_url(new moodle_url('/local/greetings/index.php'));
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading(get_string('pluginname', 'local_greetings'));
-
 echo $OUTPUT->header();
-// ...
+if (isloggedin()) {
+    echo '<h3>Greetings, ' . fullname($USER) . '</h3>';
+} else {
+    echo '<h3>Greetings, user</h3>';
+}
 echo $OUTPUT->footer();
